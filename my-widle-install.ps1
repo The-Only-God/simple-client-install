23136347e6544866623e35ff7c2a962f499f5034
The-Only-God <47788591+The-Only-God@users.noreply.github.com>
2024-11-26 00:40:58 +0100

    Create my-widle-install.ps1

---
+++ my-widle-install.ps1
@@ -0,0 +1,7 @@
+winget install -e -h --id Microsoft.PowerShell --source winget
+winget install -e -h --id Microsoft.VisualStudioCode --source winget
+winget install -e -h --id VideoLAN.VLC --source winget
+winget install -e -h --id Bitwarden.Bitwarden --source winget
+winget install -e -h --id Git.Git --source winget
+winget install -e -h --id Microsoft.AzureCLI --source winget
+Install-Module Microsoft.Graph,Az -Repository PSGallery
