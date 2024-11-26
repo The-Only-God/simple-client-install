950da85aa66e3c6d7ba2d90341f18fd02368098c
The-Only-God <47788591+The-Only-God@users.noreply.github.com>
2024-11-26 17:12:11 +0100

    Update my-widle-install.ps1

--- my-widle-install.ps1
+++ my-widle-install.ps1
@@ -4,4 +4,5 @@ winget install -e -h --id VideoLAN.VLC --source winget
 winget install -e -h --id Bitwarden.Bitwarden --source winget
 winget install -e -h --id Git.Git --source winget
 winget install -e -h --id Microsoft.AzureCLI --source winget
+winget install -e -h --id Spotify.Spotify --source winget
 Install-Module Microsoft.Graph,Az -Repository PSGallery
